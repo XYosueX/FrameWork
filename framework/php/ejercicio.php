287ed0f13a40af14f1245901766b68179671c498
Josue <69217948+XYosueX@users.noreply.github.com>
2021-05-03 23:12:02 -0500

    Add files via upload

---
+++ framework/php/ejercicio.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i = 1; $i <= 30; $i++) {
+
+    $cuadrado = ($i * $i);
+    echo "El cuadrado de ".$i. " es ".$cuadrado ;
+
+    if ($cuadrado%2 == 0) {
+        echo " es par";
+    }else {
+        echo " es impar";    }
+
+    echo"<br/>";
+}
+
+?>
